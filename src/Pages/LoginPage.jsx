2cadd5ea59e58c29ed9083a58b7768cb83e563c5
ed-coleman <edcolemancode@gmail.com>
2023-05-09 16:39:19 +0100

    change API_URL to deployed version instead of localhost

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../Contexts/SessionContext";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://hil-aked-backend.adaptable.app";
 
 function LoginPage(props) {
   const [email, setEmail] = useState("");
